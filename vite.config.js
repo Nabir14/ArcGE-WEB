c33e1afa4cb30f8f5e37f094908c0afdd7089276
Nabir14 <mushfiquefarhannabir@gmail.com>
2025-06-06 12:04:37 +0600

    0.3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "./library/arcge.js"),
+      entry: path.resolve(__dirname, "./lib/arcge.js"),
       name: "ArcGE",
       fileName: (format) => `arcge.${format}.js`,
     },
